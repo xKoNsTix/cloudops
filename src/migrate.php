b21faf8f39cec424e2cdabbbe2e7c1451eb261fe
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-23 13:38:11 +0200

    edited migrate.php

--- src/migrate.php
+++ src/migrate.php
@@ -4,7 +4,7 @@ include 'db_functions.php';
 
 $statements = [
     'CREATE TABLE IF NOT EXISTS images (
-        image_id integer NOT NULL,
+        image_id serial PRIMARY KEY,
         image_title character varying(256),
         file_name character varying(256) NOT NULL,
         image_owner character varying(256),
@@ -16,17 +16,17 @@ $statements = [
         additional_information character varying(512) NOT NULL,
         location_type character varying(512) NOT NULL,
         uploaded_at timestamp without time zone DEFAULT now(),
-        PRIMARY KEY (image_id),
         FOREIGN KEY (image_owner) REFERENCES users(username)
     )',
     'CREATE TABLE IF NOT EXISTS likes (
+        like_id serial PRIMARY KEY,
         fk_image_id integer,
         fk_image_owner character varying(256),
         FOREIGN KEY (fk_image_id) REFERENCES images(image_id),
         FOREIGN KEY (fk_image_owner) REFERENCES users(username)
     )',
     'CREATE TABLE IF NOT EXISTS users (
-        user_id integer NOT NULL,
+        user_id serial PRIMARY KEY,
         username character varying(25) NOT NULL,
         name character varying(25) NOT NULL,
         email character varying(320) NOT NULL,
@@ -35,7 +35,6 @@ $statements = [
         verified boolean DEFAULT false,
         created_at timestamp without time zone DEFAULT now(),
         updated_at timestamp without time zone DEFAULT now(),
-        PRIMARY KEY (username),
         UNIQUE (email),
         UNIQUE (name)
     )'
@@ -43,8 +42,17 @@ $statements = [
 
 try {
     $url = getenv("postgres://postgres:004dabac018008f7a6b497cdea9f8543@dokku-postgres-norvi-db:5432/norvi_db");
-    $params = parse($url);
-    $dsn = from_params($params);
+    $parsedUrl = parse_url($url);
+
+    $params = [
+        'host' => $parsedUrl['host'],
+        'port' => $parsedUrl['port'],
+        'dbname' => ltrim($parsedUrl['path'], '/'),
+        'user' => $parsedUrl['user'],
+        'password' => $parsedUrl['pass']
+    ];
+
+    $dsn = "pgsql:host={$params['host']};port={$params['port']};dbname={$params['dbname']}";
 
     // make a database connection
     $pdo = new PDO($dsn, $params['user'], $params['password'], [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
