0677c3090523549dc813d7001cdbe5a78cead33d
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-23 14:05:32 +0200

    bugfix

--- src/config.php
+++ src/config.php
@@ -14,5 +14,19 @@ if ($_SERVER['HTTP_HOST'] == 'cloudops.tokowa.at') {
     $DB_USER = "postgres"; // fill in your local db-username here!!
     $DB_PASS = "jacksparrow"; // fill in password here!!
     $DSN     = "pgsql:dbname=$DB_NAME;host=localhost";
-    echo "checkt webserver nicht"
+    echo "checkt webserver nicht";
+
 }
+// make a database connection
+try {
+    $pdo = new PDO($DSN, $DB_USER, $DB_PASS, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    // Run your database operations here
+
+    echo "Database connection successful.";
+} catch (PDOException $e) {
+    die($e->getMessage());
+} finally {
+    $pdo = null;
+}
+
