6b783e4030055dc74901e7d1957f81332aee6d6f
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-23 14:56:57 +0200

    fix

--- src/config.php
+++ src/config.php
@@ -14,7 +14,8 @@ if ($_SERVER['HTTP_HOST'] == 'cloudops.tokowa.at') {
     echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
 
  } else {
-    echo "FAIL TO ESTABLISH HOST"
+    echo "FAIL TO ESTABLISH HOST";
+    
 
 //     $DB_NAME = "norvi";
 //     $DB_USER = "postgres"; // fill in your local db-username here!!
