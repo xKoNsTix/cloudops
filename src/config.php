d78e083274a6e6b2d9fc03be067f33032f624ccd
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-23 14:25:21 +0200

    double check pw

--- src/config.php
+++ src/config.php
@@ -6,8 +6,9 @@
 if ($_SERVER['HTTP_HOST'] == 'cloudops.tokowa.at') {
     $DB_NAME = "norvi_db";
     $DB_USER = "postgres";
-    $DB_PASS = "004dabac018008f7a6b497cdea9f8543";  // fill in password here!!
+    $DB_PASS = "bilke123";  // fill in password here!!
     $DSN     = "pgsql:dbname=$DB_NAME;host=localhost";
+    echo "checkt webserver ist da";
 } else {
 
     $DB_NAME = "norvi";
