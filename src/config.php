e1d410207644d5dea45ce7f5e81696f0816458d4
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-23 15:08:31 +0200

    pdo fix

--- src/config.php
+++ src/config.php
@@ -1,8 +1,4 @@
 <?php
-
-
-
-
 if ($_SERVER['HTTP_HOST'] == 'cloudops.tokowa.at') {
     $DB_NAME = getenv('DB_NAME');
     $DB_USER = getenv('DB_USER');
@@ -10,42 +6,12 @@ if ($_SERVER['HTTP_HOST'] == 'cloudops.tokowa.at') {
     $DB_HOST = getenv('DB_HOST');
 
     $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
-
-    echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
-
- }
- if ($_SERVER['HTTP_HOST'] == 'https://cloudops.tokowa.at') {
-    $DB_NAME = getenv('DB_NAME');
-    $DB_USER = getenv('DB_USER');
-    $DB_PASS = getenv('DB_PASS');
-    $DB_HOST = getenv('DB_HOST');
-
-    $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
-
-    echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
-
- }
-
-
-
-if ($_SERVER['HTTP_HOST'] == 'http://cloudops.tokowa.at') {
-   $DB_NAME = getenv('DB_NAME');
-   $DB_USER = getenv('DB_USER');
-   $DB_PASS = getenv('DB_PASS');
-   $DB_HOST = getenv('DB_HOST');
-
-   $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
-
-   echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
-
-}
- else {
+    try {
+        $db = new PDO($DSN, $DB_USER, $DB_PASS);
+        echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+} else {
     echo "FAIL TO ESTABLISH HOST";
-
-
-//     $DB_NAME = "norvi";
-//     $DB_USER = "postgres"; // fill in your local db-username here!!
-//     $DB_PASS = "jacksparrow"; // fill in password here!!
-//     $DSN     = "pgsql:dbname=$DB_NAME;host=localhost";
-//     echo "checkt webserver nicht";
 }
