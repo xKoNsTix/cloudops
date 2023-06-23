3a9ae14e0ac969fad82ad8dc06c4293128bc479e
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-23 16:01:03 +0200

    fix

--- migrate.php
+++ migrate.php
@@ -41,11 +41,12 @@ $statements = [
 ];
 
 try {
-    $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
+
     $DB_NAME = getenv('DB_NAME');
     $DB_USER = getenv('DB_USER');
     $DB_PASS = getenv('DB_PASS');
     $DB_HOST = getenv('DB_HOST');
+    $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
 
     // make a database connection
     $db = new PDO($DSN, $DB_USER, $DB_PASS);
