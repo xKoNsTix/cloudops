e073c57d17992682f5964e3b4391871aed3096ee
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-23 15:03:47 +0200

    added alts

--- src/config.php
+++ src/config.php
@@ -13,9 +13,35 @@ if ($_SERVER['HTTP_HOST'] == 'cloudops.tokowa.at') {
 
     echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
 
- } else {
+ }
+ if ($_SERVER['HTTP_HOST'] == 'https://cloudops.tokowa.at') {
+    $DB_NAME = getenv('DB_NAME');
+    $DB_USER = getenv('DB_USER');
+    $DB_PASS = getenv('DB_PASS');
+    $DB_HOST = getenv('DB_HOST');
+
+    $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
+
+    echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
+
+ }
+
+
+
+if ($_SERVER['HTTP_HOST'] == 'http://cloudops.tokowa.at') {
+   $DB_NAME = getenv('DB_NAME');
+   $DB_USER = getenv('DB_USER');
+   $DB_PASS = getenv('DB_PASS');
+   $DB_HOST = getenv('DB_HOST');
+
+   $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
+
+   echo "<p style='color: white;'> Hallo Andi. This is an UNRELEASED VERSION. . #linuxistcool </p>";
+
+}
+ else {
     echo "FAIL TO ESTABLISH HOST";
-    
+
 
 //     $DB_NAME = "norvi";
 //     $DB_USER = "postgres"; // fill in your local db-username here!!
