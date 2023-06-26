e614825232c7186dad40e276df7e0b8b5c1fb184
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-26 12:07:17 +0200

    fix check

--- src/config.php
+++ src/config.php
@@ -1,17 +1,14 @@
 <?php
-if ($_SERVER['HTTP_HOST'] == 'norvi.tokowa.at') {
-    $DB_NAME = getenv('DB_NAME');
-    $DB_USER = getenv('DB_USER');
-    $DB_PASS = getenv('DB_PASS');
-    $DB_HOST = getenv('DB_HOST');
+$DB_NAME = getenv('DB_NAME');
+$DB_USER = getenv('DB_USER');
+$DB_PASS = getenv('DB_PASS');
+$DB_HOST = getenv('DB_HOST');
 
-    $DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
-    try {
-        $db = new PDO($DSN, $DB_USER, $DB_PASS);
-        $currentDir = getcwd();
-           } catch (PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
-    }
-} else {
-    echo "FAIL TO ESTABLISH HOST";
+$DSN = "pgsql:dbname=$DB_NAME;host=$DB_HOST";
+try {
+    $db = new PDO($DSN, $DB_USER, $DB_PASS);
+    $currentDir = getcwd();
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
+?>
