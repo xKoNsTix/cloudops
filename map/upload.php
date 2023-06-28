2562290d6770ac54c9be6f3f9c429e136a1435b3
Konstantin Kowarsch <konstantin.kowarsch@fh-salzburg.ac.at>
2023-06-28 13:24:44 +0200

    test

--- map/upload.php
+++ map/upload.php
@@ -1,38 +1,26 @@
-<!--    Salzburg University of Applied Sciences
-        Multimedia Technology
-        Multimedia Project 1
-        Creator: Konstantin Kowarsch (part-time-viking)
--->
 <?php
 
 include "../src/functions.php";
 
-if (isset($_SESSION['USER'])) { // wenn nicht eingeloggt, tschüss
+if (isset($_SESSION['USER'])) {
     $username = $_SESSION['USER'];
 } else {
     header('location:../public/index.php?message=fromAddMarkerNotLoggedIn');
 }
 
-
 $sth = $dbh->prepare("SELECT * from users where username = ?");
 $sth->execute(array($username));
-$result = $sth->fetchAll(PDO::FETCH_COLUMN, 1); // SO GEHTS!!!
+$result = $sth->fetchAll(PDO::FETCH_COLUMN, 1);
 $userName = $result[0];
 
-
-
-if (!file_exists("../img/$userName")) {         // erstelle ordner für user falls noch nicht vorhanden
-    mkdir("../img/$userName", 0777, true);
+if (!file_exists("/app/storage/$userName")) {
+    mkdir("/app/storage/$userName", 0777, true);
 }
 
-
-
-
-$target_dir = "../img/$userName/";
+$target_dir = "/app/storage/$userName/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
 
 echo $target_dir;
 // Check if image file is a actual image or fake image
@@ -43,15 +31,14 @@ if (isset($_POST["submit"])) {
         $uploadOk = 1;
     } else {
         header("Location: addMarker.php?message=File is not an image.");
-
         $uploadOk = 0;
     }
 }
+
 $filename = basename($_FILES["fileToUpload"]["name"]);
 
 // Check if file already exists
 if (file_exists($target_file)) {
-
     header("Location: addMarker.php?message=Sorry, file already exists.");
     $target_file;
     $uploadOk = 0;
